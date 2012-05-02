c39366c64e6c83df28091a19392ae73d5c6e1410
Patrick Schilf <patrick.schilf@student.hpi.uni-potsdam.de>
2012-05-02 16:21:05 +0000

    create playground and render one block, work in progress

--- js/TetrisGraphic.js
+++ js/TetrisGraphic.js
@@ -1,9 +1,48 @@
 //Raphael paper to act as playground, width and height in number of Blocks
-function TetrisGraphic(canvas, width, height) {
+function TetrisGraphic(canvas, xCount, yCount) {
   this.canvas = canvas;
+  this.playground = new Playground(canvas, xCount, yCount);
+
+  var blocks = [new Block(0, 5, 7)];
+  this.render(blocks);
+
+  this.render = function(blocks) {
+    _.each(blocks, function(block) {
+      playground.getPoint(block.x, block.y).attr({fill: '#ff0000'});
+    });
+  }
+
+
+}
+
+/*
+function PlaygroundPoint(x, y, width, height) {
+  this.x = x;
+  this.y = y;
   this.width = width;
   this.height = height;
-  
-  this.render = function(blocks) {
+
+  this.fill = function(color) {
+
+  }
+}
+*/
+
+function Playground(canvas, xCount, yCount) {
+  this.canvas = canvas;
+  this.xCount = yCount;
+  this.pWidth = canvas.width / xCount;
+  this.pHeight = canvas.height / yCount;
+  this.points = [];
+
+  _.each(_.range(xCount), function(x) {
+      points[x] = [];
+      _.each(_.range(yCount), function(y) {
+        this.points[x][y] = canvas.rect(x * this.pWidth(), y * this.pHeight, this.pWidth, this.pHeight);
+      });
+  });
+
+  this.getPoint = function(x, y) {
+    return this.points[x][y];
   }
 }
--- js/tetris.js
+++ js/tetris.js
@@ -1,6 +1,5 @@
 function Tetris(canvasName, width, height) {
   var paper = Raphael(canvasName, width, height);
-  paper.rect(0, 0, width, height).attr({fill: '#e8e8e8'});
   new TetrisLogic(new TetrisGraphic(paper, 10, 20));
 }
 
