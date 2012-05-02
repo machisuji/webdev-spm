c367e8a2b9236c9795299936bf7846bbc015cbfe
ide user ide_inkmix <ink-mix@gmx.de>
2012-05-02 15:09:56 +0000

    added Block class

--- js/tetris.js
+++ js/tetris.js
@@ -3,3 +3,11 @@ function init() {
   paper.rect(10, 10, 10, 10).attr({fill: '#e8e8e8'});
   
 }
+
+
+//x,y are int positions, type is an int specifying the color of the block
+function Block(type, x, y) {
+  this.x = x;
+  this.y = y;
+  this.type = type;
+}
