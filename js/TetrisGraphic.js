be7e9a862c2b057ec2565173b03d882fb64779ea
Patrick Schilf <patrick.schilf@student.hpi.uni-potsdam.de>
2012-05-07 16:17:33 +0000

    introduced TetrisState, to exchange relevant game state information between logic and graphic

--- js/TetrisGraphic.js
+++ js/TetrisGraphic.js
@@ -1,31 +1,112 @@
 //Raphael paper to act as playground, width and height in number of Blocks
-function TetrisGraphic(canvas, xCount, yCount) {
-  this.canvas = canvas;
-  this.playground = new Playground(canvas, xCount, yCount);
+function TetrisGraphic(canvasName, canvasRows, canvasColumns, previewName, previewRows, previewColumns) {
   this.colorMap = {0: "#f00", 1: "#0f0", 2: "#00f", 3: "#ff0", 4: "#f0f", 5: "#0ff", 6: "#000"};
+  this.canvas = new TetrisWindow(canvasName, canvasRows, canvasColumns);
+  this.preview = new TetrisWindow(previewName, previewRows, previewColumns);
+
   var self = this;
 
-  this.render = function(blocks) {
+  this.render = function(state) {
+    this.renderState(state);
+  }
+
+  this.renderState = function(state) {
     self.clear();
-    _.each(blocks, function(block) {
-      self.playground.getPoint(block.x, block.y).attr({fill: self.colorMap[block.type]});
-    });
+
+    if (state.blocks != null) {
+      this.renderBlocks(state.blocks);
+    }
+
+    if (state.active != null) {
+      this.renderBlocks(state.active);
+    }
+
+    if (state.preview != null) {
+      this.renderBlocks(state.preview);
+    }
+
   };
+
+  this.renderBlocks = function(blocks) {
+    _.each(blocks, function(block) {
+      self.canvas.getPoint(block.x, block.y).attr({fill: self.colorMap[block.type]});
+    });
+  }
+
   this.clear = function() {
-    _.each(self.playground.points, function(row) {
+    //clear canvas
+    _.each(self.canvas.points, function(row) {
+      _.each(row, function(point) {
+        point.attr({fill: '#ffffff'});
+      });
+    });
+
+    //clear preview box
+    _.each(self.preview.points, function(row) {
       _.each(row, function(point) {
         point.attr({fill: '#ffffff'});
       });
     });
   };
 
-  var blocks = [
-    new Block(2, 5, 7),
-    new Block(2, 6, 7),
-    new Block(2, 7, 7),
-    new Block(2, 8, 7)
-  ];
-  this.render(blocks);
+  /*this.createLShape = function(x, y) {
+    var blocks = {
+      center:       new Block(4, x, y - 1),
+      top:          new Block(4, x, y),
+      bottom:       new Block(4, x, y + 1),
+      bottomRight:  new Block(4, x + 1, y + 1)
+    };
+
+    blocks.center.upper = blocks.top;
+    blocks.center.lower = blocks.bottom;
+
+    blocks.top.bottom = blocks.center;
+
+    blocks.bottom.top = blocks.center;
+    blocks.bottom.right, blocks.bottomRight;
+
+    blocks.bottomRight.left, blocks.bottom;
+
+    return blocks.center;
+  };*/
+
+  var state = new TetrisState(
+    //blocks
+    [
+      new Block(3, 0, 19),
+      new Block(3, 1, 19),
+      new Block(3, 0, 18),
+      new Block(3, 1, 18),
+
+      new Block(0, 5, 17),
+      new Block(0, 5, 18),
+      new Block(0, 4, 18),
+      new Block(0, 4, 19),
+
+      new Block(1, 9, 19),
+      new Block(1, 9, 18),
+      new Block(1, 9, 17),
+      new Block(1, 8, 18),
+
+      new Block(2, 5, 19),
+      new Block(2, 6, 19),
+      new Block(2, 7, 19),
+      new Block(2, 8, 19)
+    ],
+
+    //active
+    [
+
+    ],
+
+    //preview
+    [
+
+    ],
+    0, 0, 0);
+
+  this.render(state);
+
 }
 
 /*
@@ -41,14 +122,16 @@ function PlaygroundPoint(x, y, width, height) {
 }
 */
 
-function Playground(canvas, xCount, yCount) {
-  this.canvas = canvas;
+function TetrisWindow(canvasName, xCount, yCount) {
+  var canvasElement = $("#" + canvasName);
+
+  this.canvas = Raphael(canvasName, canvasElement.width(), canvasElement.height());
   this.xCount = xCount;
   this.yCount = yCount;
   this.points = new Array(xCount);
 
-  var pWidth = canvas.width / xCount;
-  var pHeight = canvas.height / yCount;
+  var pWidth = this.canvas.width / xCount;
+  var pHeight = this.canvas.height / yCount;
   var self = this;
 
   _.each(_.range(self.xCount), function(x) {
--- js/tetris.js
+++ js/tetris.js
@@ -1,12 +1,24 @@
-function Tetris(canvasName) {
-  var paper = Raphael(canvasName, 300, 600);
-  new TetrisLogic(new TetrisGraphic(paper, 10, 20));
+function Tetris(canvasName, previewName) {
+  new TetrisLogic(new TetrisGraphic(canvasName, 10, 20, previewName, 4, 4));
 }
-
 
 //x,y are int positions, type is an int specifying the color of the block
 function Block(type, x, y) {
   this.x = x;
   this.y = y;
   this.type = type;
+  this.upper = null;
+  this.left = null;
+  this.lower = null;
+  this.right = null;
+
+}
+
+function TetrisState(blocks, active, preview, clearedLines, clearedLinesTotal, score) {
+  this.blocks = blocks;
+  this.active = active;
+  this.preview = preview;
+  this.clearedLines = clearedLines;
+  this.clearedLinesTotal = clearedLinesTotal;
+  this.score = score;
 }
